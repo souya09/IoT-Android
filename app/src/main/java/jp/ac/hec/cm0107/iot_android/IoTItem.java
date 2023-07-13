9e7def4b5d19cd2b44466f0eee2d9e31fc53487f
cmstudent <22cm0107.jec.ac.jp>
2023-07-13 12:26:49 +0900

    JsonHelper追加

---
+++ app/src/main/java/jp/ac/hec/cm0107/iot_android/IoTItem.java
@@ -0,0 +1,11 @@
+package jp.ac.hec.cm0107.iot_android;
+
+//id: "720",
+//team_id: "CM08",
+//data1: "34.50",
+//data2: null,
+//data3: null,
+//create_time: "2023-07-13 11:12:02"
+public class IoTItem {
+
+}
