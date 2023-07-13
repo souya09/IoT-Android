c745a97a49fe457cf11c54cc8465620a47eb0051
cmstudent <22cm0107.jec.ac.jp>
2023-07-13 12:12:51 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/hec/cm0107/iot_android/MainActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.hec.cm0107.iot_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
