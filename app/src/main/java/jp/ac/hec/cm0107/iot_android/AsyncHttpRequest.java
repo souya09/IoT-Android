e6662a304d8f2e823121a641030a54766a13e96d
cmstudent <22cm0107.jec.ac.jp>
2023-07-20 11:40:51 +0900

    HTTP_Req Start

---
+++ app/src/main/java/jp/ac/hec/cm0107/iot_android/AsyncHttpRequest.java
@@ -0,0 +1,105 @@
+package jp.ac.hec.cm0107.iot_android;
+
+import android.os.Handler;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+import javax.net.ssl.HttpsURLConnection;
+
+public class AsyncHttpRequest implements Runnable {
+    private Handler handler;
+    private MainActivity mainActivity;
+    private String urlStr;
+    private String resStr;
+    static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd(EE) HH:mm:ss");
+
+    public AsyncHttpRequest(Handler handler, MainActivity mainActivity, String urlStr) {
+        this.handler = handler;
+        this.mainActivity = mainActivity;
+        this.urlStr = urlStr;
+    }
+
+    @Override
+    public void run() {
+        Log.i("RssReader", "BackgroundTask start...");
+        // バックグラウンド(非同期)で実行する処理を記述する
+        // HTTP 通信の処理
+        resStr = "取得に失敗しました。";
+        HttpsURLConnection connection = null;
+
+        try {
+            //接続先 Web サイトの URL の文字列を URL クラスのオブジェクトにする
+            URL url = new URL(urlStr);
+            //接続先 Web サイトへの接続を開始
+            connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            //接続先から取得した InputStream を文字列データにする
+            resStr = inputStreamToString(connection.getInputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("AsyncHttpRequest", e.toString());
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        // 非同期処理後の処理
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                onPostExecute();
+            }
+        });
+    }
+
+    private String inputStreamToString(InputStream is) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+        br.close();
+        return sb.toString();
+    }
+
+//
+    private void onPostExecute() {
+//        Log.i("RssReader", "onPostExecute start...");
+//        // 非同期処理後に実行する処理を記述する
+//        ArrayList<IoTItem> ary = JsonHelper.parseJson(resStr);
+//        for (IoTItem tmp : ary) {
+//            mainActivity.adapter.add(tmp);
+//        }
+//        ListView list = mainActivity.findViewById(R.id.resultList);
+//
+//        list.setAdapter(mainActivity.adapter);
+//
+//        TextView txt = mainActivity.findViewById(R.id.textTime);
+//        String str = JsonHelper.pubDate;
+//
+//        SimpleDateFormat sdf =new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss", Locale.ENGLISH);
+//        try {
+//            Date date = sdf.parse(str);
+//            String dateToString = simpleDateFormat.format(date);
+//            txt.setText(dateToString);
+//        } catch (ParseException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//
+    }
+
+}
--- app/src/main/java/jp/ac/hec/cm0107/iot_android/IoTItem.java
+++ app/src/main/java/jp/ac/hec/cm0107/iot_android/IoTItem.java
@@ -8,6 +8,11 @@ package jp.ac.hec.cm0107.iot_android;
 //create_time: "2023-07-13 11:12:02"
 public class IoTItem {
     private int id;
+    private String item_id;
+    private double data1;
+    private double data2;
+    private double data3;
+    private String create_time;
 
     public int getId() {
         return id;
@@ -15,5 +20,45 @@ public class IoTItem {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getItem_id() {
+        return item_id;
+    }
+
+    public void setItem_id(String item_id) {
+        this.item_id = item_id;
+    }
+
+    public double getData1() {
+        return data1;
+    }
+
+    public void setData1(double data1) {
+        this.data1 = data1;
+    }
+
+    public double getData2() {
+        return data2;
+    }
+
+    public void setData2(double data2) {
+        this.data2 = data2;
+    }
+
+    public double getData3() {
+        return data3;
+    }
+
+    public void setData3(double data3) {
+        this.data3 = data3;
+    }
+
+    public String getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(String create_time) {
+        this.create_time = create_time;
     }
 }
